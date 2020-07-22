b10d899a4106bc01c99e5b596e519973bfe52494
BarbaraBontempo <barbarabontempo94@gmail.com>
2020-07-22 12:31:43 -0400

    Done.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,6 +1,11 @@
 class PeopleController < ApplicationController
   def new
     @person = Person.new
+    #we need this for the fields_for :addresses to appear on the page
+    #when we first create a Person they do not have an address
+    #so f.fields_for :addresses will have no address fields if there are no addresses
+    @person.addresses.build(address_type: 'work')
+    @person.addresses.build(address_type: 'home')
   end
 
   def create    
@@ -15,6 +20,16 @@ class PeopleController < ApplicationController
   private
 
   def person_params
-    params.require(:person).permit(:name)
+    params.require(:person).permit(
+      :name,
+      addresses_attributes: [
+        :street_address_1,
+        :street_address_2,
+        :city,
+        :state,
+        :zipcode,
+        :address_type
+      ]
+    )
   end
 end
